aa6bfeead70b9b618dbcd6c427919f449b61ae4a
beweb-master <fondespierre.numerique@gmail.com>
2018-06-25 14:33:47 +0200

    mise en place du depot

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// import de la classe Routing ( pour l'utiliser)
-use BWB\Framework\mvc\Routing;
-// pour beneficier de l'autoload de composer
-include "vendor/autoload.php";
-
-// A chaque requete emise nous lançons le mecanisme de routage
-(new Routing())->execute();
